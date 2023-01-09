a5743c5f8a6a716210411695ddbffd6d7cd2e3a4
Fifinella <h.hoesley@gmail.com>
2023-01-08 19:51:47 -0800

    index.js

--- Develop/index.js
+++ Develop/index.js
@@ -11,3 +11,28 @@ function init() {}
 
 // Function call to initialize app
 init();
+// Variables containing required packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+// Variables to connect modules to application
+const generateMarkdown = require("./js/gen");
+const licenseBadge = require("./js/lic").licenseBadge;
+const questions = require("./js/quest").questions;
+//Allows for use of async await
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// function to initialize program and create README file
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    answers.licenseBadge = licenseBadge(answers.license);
+    let readMeData = generateMarkdown(answers);
+    await writeFileAsync("created-README.md", readMeData);
+  } catch (err) {
+    throw err;
+  }
+}
+
+// function call to initialize program
+init();
